e57365909a54e50f69d0634dc4e85c7126a72cff
BhavtoshRath <bhavtoshrath.umn@gmail.com>
2018-09-30 19:02:22 -0500

    commit for 30-9-18(2)

--- LinkedList/implementation.py
+++ LinkedList/implementation.py
@@ -16,5 +16,21 @@ class Node:
         self.next = new_next
 
 
+class LinkedList:
+    def __init__(self):
+        self.head = None
+
+    def isEmpty(self):
+        return self.head == None
+
+    def add(self, data):  # 'head' is a pointer, not a node!!!
+        node = Node(data)
+        node.setNext(self.head)
+        self.head = node
+
+
+
+
+
 n = Node(5)
 print(n.getData())
